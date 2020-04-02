a9d767b482b1df8bd27211ebc712ce793a22c7f1
patito <p.sotosellanes@hotmail.com>
2020-04-01 23:52:50 -0300

    finishing post CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
 
-  
   def create
     @post = Post.new(post_params)
     
@@ -26,12 +26,23 @@ class PostsController < ApplicationController
   end
   
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit" 
+    end   
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path 
   end
 
   private
+  def find_post
+    @post = Post.find(params[:id])
+  end   
+  
   def post_params
     params.require(:post).permit(:title, :body)
   end  
